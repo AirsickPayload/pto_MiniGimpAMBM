04238fd6592129704185f1de7197488585360060
Alan Matuszczak <al4n00@gmail.com>
2015-10-15 14:15:07 +0200

    Dodanie kowersji na skalę odcieni szarości.

--- src/core/transformations/conversion_grayscale.cpp
+++ src/core/transformations/conversion_grayscale.cpp
@@ -21,11 +21,30 @@ PNM* ConversionGrayscale::transform()
 
     if (image->format() == QImage::Format_Mono)
     {
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QColor color = QColor::fromRgb(image->pixel(x,y)); // Getting the pixel(x,y) value
+
+                newImage->setPixel(x,y, color == Qt::black ? PIXEL_VAL_MAX : PIXEL_VAL_MIN);
+            }
 
     }
     else // if (image->format() == QImage::Format_RGB32)
     {
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
 
+                int r = qRed(pixel);    // Get the 0-255 value of the R channel
+                int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+                int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+
+                float srednia = 0.3 * r + 0.6 * g + 0.1 * b;
+
+                newImage->setPixel(x,y, srednia);
+            }
     }
 
     return newImage;
