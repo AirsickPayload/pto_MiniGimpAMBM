291140f4a77553a3486d930c1fdd2f378ba489bf
Alan Matuszczak <al4n00@gmail.com>
2015-11-15 20:04:37 +0100

    *ZADANIE 6 *Implementacja filtru medianowego. *Implementacja filtru bilateralnego.

--- src/core/transformations/noise_bilateral.cpp
+++ src/core/transformations/noise_bilateral.cpp
@@ -21,28 +21,47 @@ PNM* NoiseBilateral::transform()
     sigma_r = getParameter("sigma_r").toInt();
     radius = sigma_d;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    if(image->format() == QImage::Format_Indexed8){
+        for(int x = 0; x < width; x++){
+            for(int y  = 0; y < height; y++){
+                newImage -> setPixel(x, y, calcVal(x, y, LChannel));
+            }
+        }
+    } else if(image->format() == QImage::Format_RGB32){
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                newImage -> setPixel(x, y, qRgb(calcVal(x, y, RChannel),
+                                                calcVal(x, y, GChannel),
+                                                calcVal(x, y, BChannel)));
+            }
+        }
+    }
 
     return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    int size = radius;
+    math::matrix<float> okno = getWindow(x, y, size, channel, RepeatEdge);
+    float sumaL = 0, sumaM = 0, colorSpatC = 0;
 
-    return 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            colorSpatC = colorCloseness(okno(i, j), okno(size/2, size/2)) * spatialCloseness(QPoint(i, j), QPoint(radius/2, radius/2));
+            sumaL += okno(i, j) * colorSpatC;
+            sumaM += colorSpatC;
+        }
+    }
+    return (int)(sumaL / sumaM);
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    return exp(pow((val1-val2), 2) / (2 * pow(sigma_r, 2) * (-1)));
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    return exp((pow(point1.x()-point2.x(), 2) + pow(point1.y()-point2.y(), 2)) / (2 * pow(sigma_d, 2) * (-1)));
 }
--- src/core/transformations/noise_median.cpp
+++ src/core/transformations/noise_median.cpp
@@ -17,16 +17,41 @@ PNM* NoiseMedian::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
+    if(image->format() == QImage::Format_Indexed8){
+        for(int x = 0; x < width; x++){
+            for(int y  = 0; y < height; y++){
+                newImage -> setPixel(x, y, getMedian(x, y, LChannel));
+            }
+        }
+    } else if(image->format() == QImage::Format_RGB32){
+            for(int x = 0; x < width; x++){
+                for(int y = 0; y < height; y++){
+                    newImage -> setPixel(x, y, qRgb(getMedian(x, y, RChannel),
+                                                    getMedian(x, y, GChannel),
+                                                    getMedian(x, y, BChannel)));
+                }
+            }
+    }
     return newImage;
 }
 
 int NoiseMedian::getMedian(int x, int y, Channel channel)
 {
     int radius = getParameter("radius").toInt();
+    int size = radius * 2  + 1;
+    math::matrix<float> okno = getWindow(x, y, size, channel, RepeatEdge);
+    float* pomTabObl = new float[size * size];
+    int l = 0;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            pomTabObl[l] = okno(i, j);
+            l++;
+        }
+    }
 
-    return 0;
+    //Sortowanie przed pobraniem mediany
+    std::sort(pomTabObl, pomTabObl + size * size);
+
+    return (int)pomTabObl[(int)(floor(size * size) / 2)];
 }
